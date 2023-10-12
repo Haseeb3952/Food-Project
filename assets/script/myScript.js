e34df86b9bc51fc730355a04a8ac9bf4de228bef
Abdul Haseeb <haseeb7063952@gmail.com>
2023-10-12 12:37:12 +0500

    Food project

---
+++ assets/script/myScript.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var navbar = document.querySelector('.navbar');
+  
+    window.addEventListener('scroll', function () {
+      // Check if the page has been scrolled
+      if (window.scrollY > 0) {
+        // If scrolled, add the "scrolled" class to change the background color
+        navbar.classList.add('scrolled');
+        // Add the "shadow" class to apply a subtle box shadow
+        navbar.classList.add('shadow');
+      } else {
+        // If at the top, remove the "scrolled" class to restore the transparent background
+        navbar.classList.remove('scrolled');
+        // Remove the "shadow" class to remove the box shadow
+        navbar.classList.remove('shadow');
+      }
+    });
+  });
+  
